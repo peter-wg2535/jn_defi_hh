89eac72040b57c427eea4d8188f4dba12d81a3c1
technqvi <38780060+technqvi@users.noreply.github.com>
2022-07-28 11:24:38 +0700

    ok

--- deploys/treasury-defi-deploy.js
+++ deploys/treasury-defi-deploy.js
@@ -1,6 +1,7 @@
 const hre = require("hardhat");
 async function main() {
-
+//https://rinkeby.etherscan.io/address/0x1B6891E4Cfd948d7Efc674F13Decad3F44bB65a7#code
+//https://rinkeby.etherscan.io/address/0x8eEb30D5163D6513Fb636961852b765558D8e3d3#code
   const chain_network='rinkeby'
   const Treasury = await hre.ethers.getContractFactory("DualTreasuryDefi");
   const treasury = await Treasury.deploy(1000000,10
